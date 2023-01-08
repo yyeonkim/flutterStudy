244dbec6e38dc4951cc0324c997eae68f0b67d0d
yyeonkim <seyeon.dev@gmail.com>
2023-01-08 20:00:45 +0900

    pomodoros theme 함수 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_study/screens/home_screen.dart';
+import 'package:flutter_study/utils/theme.dart';
 
 void main() {
   runApp(App());
@@ -9,15 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        backgroundColor: const Color(0xFFE7626C),
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            color: Color(0xff232b55),
-          ),
-        ),
-        cardColor: const Color(0xfff4eddb),
-      ),
+      theme: pomodoroTheme(),
       home: const HomeScreen(),
     );
   }
---
+++ lib/utils/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData pomodoroTheme() {
+  return ThemeData(
+    backgroundColor: const Color(0xFFE7626C),
+    textTheme: const TextTheme(
+      headline1: TextStyle(
+        color: Color(0xff232b55),
+      ),
+    ),
+    cardColor: const Color(0xfff4eddb),
+  );
+}
