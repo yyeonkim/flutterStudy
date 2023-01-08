b2e0721fd4eb1d3021c476066e51b9e74795cdae
yyeonkim <seyeon.dev@gmail.com>
2023-01-08 17:31:34 +0900

    Add wallet widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study/widgets/button.dart';
+import 'package:flutter_study/widgets/wallet.dart';
 
 void main() {
   runApp(App());
@@ -8,80 +8,6 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          backgroundColor: const Color(0xFF181818),
-          body: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          const Text(
-                            "Hey, Selena",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 3,
-                          ),
-                          Text(
-                            "Welcome back",
-                            style: TextStyle(
-                                color: Colors.white.withOpacity(0.6),
-                                fontSize: 16),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 120,
-                  ),
-                  Text(
-                    "Total Balance",
-                    style: TextStyle(
-                      color: Colors.white.withOpacity(0.6),
-                      fontSize: 20,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    "\$5 194 482",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 32,
-                      fontWeight: FontWeight.w800,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Button(
-                          text: "Transfer",
-                          bgColor: Colors.amber,
-                          textColor: Colors.black),
-                      Button(
-                          text: "Request",
-                          bgColor: Color(0xFF1F2123),
-                          textColor: Colors.white)
-                    ],
-                  )
-                ],
-              ))),
-    );
+    return const Wallet();
   }
 }
