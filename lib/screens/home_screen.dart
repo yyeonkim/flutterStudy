206cb607610ff5b291be3830deece248d9fc21f1
yyeonkim <seyeon.dev@gmail.com>
2023-01-08 20:00:32 +0900

    Pomodoros 위젯 생성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,108 +1,12 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+import 'package:flutter_study/widgets/pomodoros.dart';
+
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int totalSeconds = 1500;
-  bool isRunning = false;
-  late Timer timer;
-
-  void onTick(Timer timer) {
-    setState(() {
-      totalSeconds -= 1;
-    });
-  }
-
-  void onStartPressed() {
-    timer = Timer.periodic(const Duration(seconds: 1), onTick);
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  void onPausePressed() {
-    timer.cancel();
-    setState(() {
-      isRunning = false;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Column(children: [
-        Flexible(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                "$totalSeconds",
-                style: TextStyle(
-                  color: Theme.of(context).cardColor,
-                  fontSize: 90,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            )),
-        Flexible(
-            flex: 3,
-            child: Center(
-              child: IconButton(
-                onPressed: isRunning ? onPausePressed : onStartPressed,
-                iconSize: 120,
-                color: Theme.of(context).cardColor,
-                icon: Icon(isRunning
-                    ? Icons.play_circle_outline
-                    : Icons.pause_circle_outline),
-              ),
-            )),
-        Flexible(
-            flex: 1,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(50),
-                        topRight: Radius.circular(50),
-                      ),
-                      color: Theme.of(context).cardColor,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Pomodoros",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Theme.of(context).textTheme.headline1!.color,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          "0",
-                          style: TextStyle(
-                            fontSize: 50,
-                            color: Theme.of(context).textTheme.headline1!.color,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ))
-      ]),
-    );
+    return const Pomodoros();
   }
 }
---
+++ lib/widgets/pomodoros.dart
@@ -0,0 +1,123 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class Pomodoros extends StatefulWidget {
+  const Pomodoros({super.key});
+
+  @override
+  State<Pomodoros> createState() => _PomodorosState();
+}
+
+class _PomodorosState extends State<Pomodoros> {
+  static const twentyFiveMinutes = 1500;
+  int totalSeconds = twentyFiveMinutes;
+  bool isRunning = false;
+  int totalPomodoros = 0;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros += 1;
+        isRunning = false;
+        totalSeconds = twentyFiveMinutes;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds -= 1;
+      });
+    }
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    return duration.toString().split(".").first.substring(2);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Column(children: [
+        Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                format(totalSeconds),
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 90,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            )),
+        Flexible(
+            flex: 3,
+            child: Center(
+              child: IconButton(
+                onPressed: isRunning ? onPausePressed : onStartPressed,
+                iconSize: 120,
+                color: Theme.of(context).cardColor,
+                icon: Icon(isRunning
+                    ? Icons.pause_circle_outline
+                    : Icons.play_circle_outline),
+              ),
+            )),
+        Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50),
+                      ),
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Pomodoros",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Theme.of(context).textTheme.headline1!.color,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          "$totalPomodoros",
+                          style: TextStyle(
+                            fontSize: 50,
+                            color: Theme.of(context).textTheme.headline1!.color,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ))
+      ]),
+    );
+  }
+}
