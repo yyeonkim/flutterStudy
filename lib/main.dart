8930c27891c373d5af2b235bcd5ba761311dea17
yyeonkim <seyeon.dev@gmail.com>
2023-01-08 16:38:17 +0900

    Feat: Add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Color(0xFF181818),
+          backgroundColor: const Color(0xFF181818),
           body: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
@@ -20,13 +21,16 @@ class App extends StatelessWidget {
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
-                          Text(
+                          const Text(
                             "Hey, Selena",
                             style: TextStyle(
                               color: Colors.white,
                               fontSize: 24,
-                              fontWeight: FontWeight.w800,
+                              fontWeight: FontWeight.w600,
                             ),
+                          ),
+                          const SizedBox(
+                            height: 3,
                           ),
                           Text(
                             "Welcome back",
@@ -36,6 +40,66 @@ class App extends StatelessWidget {
                           ),
                         ],
                       )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 120,
+                  ),
+                  Text(
+                    "Total Balance",
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.6),
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "\$5 194 482",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 165,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(50)),
+                        child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 16,
+                            ),
+                            child: Text(
+                              "Transfer",
+                              style: TextStyle(fontSize: 20),
+                            )),
+                      ),
+                      Container(
+                        width: 165,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: const Color(0xFF1F2123),
+                            borderRadius: BorderRadius.circular(50)),
+                        child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 16,
+                            ),
+                            child: Text(
+                              "Request",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            )),
+                      ),
                     ],
                   )
                 ],
