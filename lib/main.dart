8e218f70b922ca8cc0e01e5e51478d3fd417f6f2
yyeonkim <seyeon.dev@gmail.com>
2023-01-08 15:14:39 +0900

    Feat: Add Header

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,37 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello flutter!"),
-        ),
-        body: Center(
-          child: Text("Hello flutter!"),
-        ),
-      ),
+          backgroundColor: Color(0xFF181818),
+          body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Hey, Selena",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          Text(
+                            "Welcome back",
+                            style: TextStyle(
+                                color: Colors.white.withOpacity(0.6),
+                                fontSize: 16),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ))),
     );
   }
 }
