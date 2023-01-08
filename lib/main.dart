8bf8451668702630293b6ea429c8335376e4c36e
yyeonkim <seyeon.dev@gmail.com>
2023-01-08 17:48:57 +0900

    StatefulWidget 사용하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study/widgets/wallet.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onPressed() {
+    setState(() {
+      counter += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Wallet();
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: const Color(0xFFF4EDDB),
+          body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text(
+                "Click count",
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                "$counter",
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                  iconSize: 40,
+                  onPressed: onPressed,
+                  icon: const Icon(Icons.add_box_rounded))
+            ]),
+          )),
+    );
   }
 }
